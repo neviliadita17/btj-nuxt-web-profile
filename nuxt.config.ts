b0cd1ded432a9412d39760bc8633239b3726950e
Neviliadita Berthilydia <61975618+neviliadita17@users.noreply.github.com>
2024-01-04 21:02:48 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,34 +11,14 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  
-  // router: {
-  //   base: '/',
-  //   extendRoutes(routes, resolve) {
-  //     routes.push({
-  //       name: 'index',
-  //       path: '/',
-  //       component: resolve(__dirname, 'pages/index.vue'),
-  //     });
-  //   },
-  // },
-  // router: {
-  //   linkActiveClass: 'active',
-  //   extendRoutes(routes, resolve) {
-  //     routes.push(
-  //       {
-  //         path: '/admin/about-me',
-  //         component: resolve(__dirname, 'pages/admin/about-me.vue'),
-  //       },
-  //       {
-  //         path: '/admin/edu-work',
-  //         component: resolve(__dirname, 'pages/admin/edu-work.vue'),
-  //       },
-  //       {
-  //         path: '/admin/motto',
-  //         component: resolve(__dirname, 'pages/admin/motto.vue'),
-  //       }
-  //     );
-  //   },
-  // },
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap',
+        },
+      ],
+    },
+  }
 })
